be79325cb913c58ae6d20a98e2103a5b56c60410
Sergey <non_2001@mail.ru>
2016-04-12 13:02:44 +0300

    cpp-test adapted for emscripten

--- tests/cpp-tests/Classes/ExtensionsTest/ExtensionsTest.cpp
+++ tests/cpp-tests/Classes/ExtensionsTest/ExtensionsTest.cpp
@@ -4,7 +4,7 @@
 #include "NotificationCenterTest/NotificationCenterTest.h"
 #include "ControlExtensionTest/CCControlSceneManager.h"
 #include "CocosBuilderTest/CocosBuilderTest.h"
-#if (CC_TARGET_PLATFORM != CC_PLATFORM_EMSCRIPTEN) && (CC_TARGET_PLATFORM != CC_PLATFORM_NACL)
+#if (CC_TARGET_PLATFORM != CC_PLATFORM_NACL)
 #include "NetworkTest/HttpClientTest.h"
 #endif
 #include "TableViewTest/TableViewTestScene.h"
@@ -15,8 +15,12 @@
 #include "CocoStudioSceneTest/SceneEditorTest.h"
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS) || (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-#include "NetworkTest/WebSocketTest.h"
 #include "NetworkTest/SocketIOTest.h"
+#include "NetworkTest/WebSocketTest.h"
+#endif
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_EMSCRIPTEN)
+#include "NetworkTest/WebSocketTest.h"
 #endif
 
 ExtensionsTests::ExtensionsTests()
@@ -25,12 +29,15 @@ ExtensionsTests::ExtensionsTests()
     addTest("NotificationCenterTest", [](){ return new (std::nothrow) NotificationCenterTests; });
     addTest("CCControlButtonTest", [](){ return new (std::nothrow) ControlTests; });
     addTest("CocosBuilderTest", [](){ return new (std::nothrow) CocosBuilderTests; });
-#if (CC_TARGET_PLATFORM != CC_PLATFORM_EMSCRIPTEN) && (CC_TARGET_PLATFORM != CC_PLATFORM_NACL)
+#if (CC_TARGET_PLATFORM != CC_PLATFORM_NACL)
     addTest("HttpClientTest", [](){ return new (std::nothrow) HttpClientTests; });
 #endif
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS) || (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
     addTest("WebSocketTest", [](){ return new (std::nothrow) WebSocketTests; });
     addTest("SocketIOTest", [](){ return new (std::nothrow) SocketIOTests; });
+#endif
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_EMSCRIPTEN)
+    addTest("WebSocketTest", [](){ return new (std::nothrow) WebSocketTests; });
 #endif
     addTest("TableViewTest", [](){ return new (std::nothrow) TableViewTests; });
     addTest("CocoStudioArmatureTest", [](){ return new (std::nothrow) CocoStudioArmatureTests; });
