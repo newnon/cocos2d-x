084abdd8dbe5b97ae60e42dd9577a4fe9c249d34
Sergey <non_2001@mail.ru>
2018-01-15 19:56:09 +0300

    Widget params properties

--- cocos/editor-support/spritebuilder/CCBXWidgetLoader.h
+++ cocos/editor-support/spritebuilder/CCBXWidgetLoader.h
@@ -19,14 +19,13 @@ CC_CONSTRUCTOR_ACCESS:
 
 protected:
     virtual void onHandlePropTypeClickCallback(const std::string &propertyName, bool isExtraProp, const CallbackDescription &value) override;
-    virtual void onHandlePropTypeTouchCallback(const std::string &propertyName, bool isExtraProp, const CallbackDescription &value) override;
     virtual void onHandlePropTypeCheck(const std::string &propertyName, bool isExtraProp, bool value) override;
     virtual void onHandlePropTypeSoundFile(const std::string &propertyName, bool isExtraProp, const std::string &value) override;
     virtual void setCallbacks(Node* node, CCBXReaderOwner *owner, Node *rootNode, CCBXReaderOwner *parentOwner, const NodeParams& params) const override;
     
 private:
-    CallbackDescription _touch;
-    CallbackDescription _click;
+    CallbackDescription _touchDesc;
+    CallbackDescription _clickDesc;
     std::string _sound;
     bool _enabled;
 };
